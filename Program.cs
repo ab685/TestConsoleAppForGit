a879a7d1754dcadc0d2894872a75a212cfb35b85
Abhishek685 <ashah@datasostech.com>
2024-02-26 18:00:45 +0530

    this is statached code into master

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace TestConsoleAppForGit
         static void DoSomeThing()
         {
             
-            Console.WriteLine("Hello World! This Development branch");
+            Console.WriteLine("Hello World! This Test of stash  Development branch");
             Console.ReadLine();
         }
 
