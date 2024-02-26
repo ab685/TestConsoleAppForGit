e922f193938c5e3fa2a8e850c98d95b17aeb7172
Abhishek685 <ashah@datasostech.com>
2024-02-26 17:17:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestConsoleAppForGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DoSomeThing();
+        }
+        static void DoSomeThing()
+        {
+            Console.WriteLine("Hello World! This Master branch");
+            Console.ReadLine();
+        }
+
+    }
+}
